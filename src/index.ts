2fe3347c8a819808a02c1f57c3631aa417a16a7a
ali6200 <atirmizi6200@gmail.com>
2024-07-26 21:12:42 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+require('dotenv').config()
+
+const app = express()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
+
+app.get('/', (req, res) => {res.send({"msg": "Chai"})})
